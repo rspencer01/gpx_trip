cd7a2659fa9c551ade315435016e4047b1904882
Robert Spencer <general@robertandrewspencer.com>
2020-08-30 15:56:05 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="gpx_trip",
     description="Extract stops and journeys from gpx traces",
-    version="0.0.2dev",
+    version="0.0.3dev",
     author="R. A. Spencer",
     author_email="general@robertandrewspencer.com",
     packages=["gpx_trip"],
