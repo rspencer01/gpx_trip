608bb02faca180d3f313d38641928a18ce1285ae
Robert Spencer <general@robertandrewspencer.com>
2021-01-17 12:03:57 +0200

    Update copyrights

--- gpx_trip/__init__.py
+++ gpx_trip/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020  R. A. Spencer
+# Copyright (C) 2021  R. A. Spencer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
